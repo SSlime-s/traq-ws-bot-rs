958a2e98daf600ed013ca6f98bd9943ea7b72939
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-08-16 10:47:48 +0900

    ♻️ examples ディレクトリに全部まとめるようにした

--- bot-core/src/utils.rs
+++ bot-core/src/utils.rs
@@ -1,5 +1,6 @@
 use crate::events::common::Message;
 
+/// メッセージに `target_user_id` に対するメンションが含まれているかどうかを返す
 pub fn is_mentioned_message(message: &Message, target_user_id: &str) -> bool {
     message
         .embedded
