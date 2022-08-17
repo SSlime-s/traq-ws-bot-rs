bf0d686ada6945c752343f540dbeaac63d4a3b57
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-08-18 01:52:36 +0900

    ✨ example 動く状態にした

--- examples/echo-bot/src/main.rs
+++ examples/echo-bot/src/main.rs
@@ -1,4 +1,4 @@
-use traq_ws_bot_rs::bot::TraqBot;
+use traq_ws_bot::bot::builder;
 
 #[tokio::main]
 async fn main() {
@@ -6,8 +6,11 @@ async fn main() {
 
     let bot_access_token = std::env::var("BOT_ACCESS_TOKEN").unwrap_or_else(|_| String::new());
 
-    let bot = TraqBot::new(bot_access_token, "ws://localhost:3000").on_message_created(|event| {
-        println!("{:?}", event);
-    });
+    let bot = builder(bot_access_token)
+        .on_direct_message_created(|event| {
+            println!("{:?}", event);
+        })
+        .build();
+
     bot.start().await.unwrap();
 }
