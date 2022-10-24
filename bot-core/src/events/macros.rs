7b262ba05d334c22e5a97c2bee207d7deac97e2b
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-10-25 00:21:33 +0900

    🗑️ いらなくなったコメントアウト

--- bot-core/src/events/macros.rs
+++ bot-core/src/events/macros.rs
@@ -1,28 +1,6 @@
 /// 汎用的な derive と 全 field に pub をつけるマクロ
 /// field 名に as を用いることで、serialize/deserialize に変換する際のキーを指定できる
 macro_rules! gp_struct {
-    // ($name:ident { $( $field:ident $(as $rename:tt)? : $type:ty ),* $(,)? }) => {
-    //     #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-    //     pub struct $name {
-    //         $(
-    //             $(#[serde(rename = $rename)])?
-    //             pub $field: $type,
-    //         )*
-    //     }
-    // };
-
-    // ($(struct $name:ident { $( $field:ident $(as $rename:tt)? : $type:ty ),* $(,)? })+) => {
-    //     $(
-    //         #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-    //         pub struct $name {
-    //             $(
-    //                 $(#[serde(rename = $rename)])?
-    //                 pub $field: $type,
-    //             )*
-    //         }
-    //     )+
-    // };
-
     ($(struct $name:ident {  $( $(#[$attr:meta])* $field:ident: $type:ty ),* $(,)? })+) => {
         $(
             #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
