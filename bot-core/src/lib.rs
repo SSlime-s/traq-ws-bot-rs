427282f7de914b450dcad066639e5e9bc5c56999
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-11-19 01:14:13 +0900

    🗑️ lib.rs まともにした

--- bot-core/src/lib.rs
+++ bot-core/src/lib.rs
@@ -2,29 +2,10 @@
 //!
 
 pub mod bot;
+pub use bot::builder;
 pub mod events;
 pub mod utils;
 
 #[cfg(feature = "openapi")]
 #[doc(inline)]
 pub use openapi;
-
-#[cfg(test)]
-mod tests {
-    use crate::events::{self, common::Base, payload::TagRemoved, Events};
-
-    #[test]
-    fn it_works() {
-        let events = Events::TagRemoved(TagRemoved {
-            base: Base {
-                event_time: "2020-01-01T00:00:00.000Z".parse().unwrap(),
-            },
-            tag_id: "tag_id_tmp".to_string(),
-            tag: "tag_name_tmp".to_string(),
-        });
-        assert_eq!(
-            format!("{}", serde_json::to_string(&events).unwrap()),
-            String::new()
-        );
-    }
-}
