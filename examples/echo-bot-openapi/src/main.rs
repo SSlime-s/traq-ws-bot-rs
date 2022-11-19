12e5fe42b66b5704d77572f897ad959f43bc7a0f
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-11-19 17:41:41 +0900

    💚 examples に RateLimiter 入れた

--- examples/echo-bot-openapi/src/main.rs
+++ examples/echo-bot-openapi/src/main.rs
@@ -1,24 +1,39 @@
 use traq_ws_bot::{
     bot::builder,
     openapi::{self, models::PostMessageRequest},
-    utils::create_configuration,
+    utils::{create_configuration, RateLimiter},
 };
+
+struct Resource {
+    bot_access_token: String,
+    limiter: RateLimiter,
+}
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
 
     let bot_access_token = std::env::var("BOT_ACCESS_TOKEN").unwrap_or_else(|_| String::new());
+    let limiter = RateLimiter::default();
+    let resource = Resource {
+        bot_access_token: bot_access_token.clone(),
+        limiter,
+    };
 
     let bot = builder(&bot_access_token)
-        .insert_resource(bot_access_token.clone())
+        .insert_resource(resource)
         .on_direct_message_created_with_resource(|event, resource| async move {
             let post_message_request = PostMessageRequest {
                 content: event.message.plain_text,
                 embed: Some(false),
             };
 
-            let configuration = create_configuration(resource.as_ref());
+            let configuration = create_configuration(&resource.bot_access_token);
+
+            if !resource.limiter.try_acquire().await {
+                dbg!("Rate limit exceeded");
+                return;
+            }
 
             let res = openapi::apis::message_api::post_message(
                 &configuration,
--- examples/echo-bot/src/main.rs
+++ examples/echo-bot/src/main.rs
@@ -1,18 +1,28 @@
 use traq_ws_bot::{
     bot::{builder, TRAQ_ORIGIN},
-    utils::create_client,
+    utils::{create_client, RateLimiter},
 };
+
+struct Resource {
+    bot_access_token: String,
+    limiter: RateLimiter,
+}
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
 
     let bot_access_token = std::env::var("BOT_ACCESS_TOKEN").unwrap_or_else(|_| String::new());
+    let limiter = RateLimiter::default();
+    let resource = Resource {
+        bot_access_token: bot_access_token.clone(),
+        limiter,
+    };
 
     let bot = builder(&bot_access_token)
-        .insert_resource(bot_access_token.clone())
+        .insert_resource(resource)
         .on_direct_message_created_with_resource(|event, resource| async move {
-            let client = create_client(resource.as_ref());
+            let client = create_client(resource.bot_access_token.as_str());
             let url = format!(
                 "{}/api/v3/channels/{}/messages",
                 TRAQ_ORIGIN, event.message.channel_id
@@ -22,6 +32,11 @@ async fn main() {
                 "content": event.message.plain_text,
                 "embed": false
             });
+
+            if !resource.limiter.try_acquire().await {
+                dbg!("Rate limit exceeded");
+                return;
+            }
 
             let res = client.post(&url).json(&body).send().await.unwrap();
             let json = res.json::<serde_json::Value>().await.unwrap();
